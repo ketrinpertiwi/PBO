34823f51f0096cf97c76e210be68b0bb03198829
ketrinpertiwi <ketrinpertiwi26@gmail.com>
2025-12-09 21:01:59 +0700

    Update Pendonor.java

--- bloodcare/Pendonor.java
+++ bloodcare/Pendonor.java
@@ -1,1 +1,95 @@
+package com.bloodcare.bloodcare;
+
+import java.time.LocalDate;
+
+public class Pendonor {
+    private int id;
+    private String nama;
+    private String nik;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin; 
+    private String alamat;
+    private String telepon;
+    private String golDarah;     
+
+    public Pendonor() {
+    }
+
+    public Pendonor(int id, String nama, String nik, LocalDate tanggalLahir,
+                    String jenisKelamin, String alamat, String telepon, String golDarah) {
+        this.id = id;
+        this.nama = nama;
+        this.nik = nik;
+        this.tanggalLahir = tanggalLahir;
+        this.jenisKelamin = jenisKelamin;
+        this.alamat = alamat;
+        this.telepon = telepon;
+        this.golDarah = golDarah;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNik() {
+        return nik;
+    }
+
+    public void setNik(String nik) {
+        this.nik = nik;
+    }
+
+    public LocalDate getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(LocalDate tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public void setTelepon(String telepon) {
+        this.telepon = telepon;
+    }
+
+    public String getGolDarah() {
+        return golDarah;
+    }
+
+    public void setGolDarah(String golDarah) {
+        this.golDarah = golDarah;
+    }
+}
+
 
