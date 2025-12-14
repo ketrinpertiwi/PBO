5a81bab99e04f92bc0374366731a76c181fafd56
ketrinpertiwi <ketrinpertiwi26@gmail.com>
2025-12-14 21:08:20 +0700

    Add StokDarah class with properties and methods

--- bloodcare/StokDarah.java
+++ bloodcare/StokDarah.java
@@ -1,1 +1,14 @@
+package com.bloodcare.bloodcare;
 
+public class StokDarah {
+    private int id;
+    private String golDarah;
+    private String rhesus;
+    private int jumlah;
+
+    public StokDarah() {}
+    public int getId() { return id; } public void setId(int id) { this.id = id; }
+    public String getGolDarah() { return golDarah; } public void setGolDarah(String golDarah) { this.golDarah = golDarah; }
+    public String getRhesus() { return rhesus; } public void setRhesus(String rhesus) { this.rhesus = rhesus; }
+    public int getJumlah() { return jumlah; } public void setJumlah(int jumlah) { this.jumlah = jumlah; }
+}
